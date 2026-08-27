e1660f59657d870c1550c25335059996a1270819
Ajithkumar Nagarajan <n.ajithkumar17@gmail.com>
2026-08-27 07:27:43 +0530

    Add files via upload

---
+++ Task1.js
@@ -0,0 +1,25 @@
+// Task 1: Create variables for student name, age, and mark
+let studentName = "Ajith";
+let studentAge = 30;
+let studentMark = 85;
+console.log("Student Name:", studentName);
+console.log("Age:", studentAge);
+console.log("Mark:", studentMark);
+
+// Task 2: Ask user's name using prompt() and display using alert()
+let name = prompt("Enter your name:");
+alert("Your name is: " + name);
+
+// Task 3: Ask user's age using prompt() and print using console.log()
+let age = prompt("Enter your age:");
+console.log("Your age is:", age);
+
+
+// Task 4: Ask the user a question using confirm()
+let result = confirm("Do you like JavaScript?");
+console.log("Your answer is:", result);
+
+
+// Task 5: Ask user's name and display it using document.writeln()
+let userName = prompt("Enter your name:");
+document.writeln("Hello " + userName);
